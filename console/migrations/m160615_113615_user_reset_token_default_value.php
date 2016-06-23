631be92190a660617050ad4298497e72d8ca50a2
Dmitriy Raschepkin <dmitriy.raschepkin@gmail.com>
2016-06-23 17:38:11 +0300

    down migration fix

--- console/migrations/m160615_113615_user_reset_token_default_value.php
+++ console/migrations/m160615_113615_user_reset_token_default_value.php
@@ -4,13 +4,16 @@ use yii\db\Migration;
 
 class m160615_113615_user_reset_token_default_value extends Migration
 {
+    protected $tableName = '{{%user}}';
+
     public function up()
     {
-        $this->alterColumn('{{%user}}', 'verification_token', $this->string()->after('password_reset_token'));
+        $this->alterColumn($this->tableName, 'verification_token', $this->string()->after('password_reset_token'));
     }
 
     public function down()
     {
-        $this->alterColumn('{{%user}}', 'verification_token', $this->string()->notNull()->after('password_reset_token'));
+        $this->delete($this->tableName, ['verification_token' => null]);
+        $this->alterColumn($this->tableName, 'verification_token', $this->string()->notNull()->after('password_reset_token'));
     }
 }
